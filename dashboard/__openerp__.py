be2ab912a14d8ed89ca6cc0ae041b851e1d153cc
AkbarAlidev <76706187+AkbarAlidev@users.noreply.github.com>
2021-01-14 13:20:58 +0530

    new1

---
+++ dashboard/__openerp__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Dashboard",
+    "version": "1.0",
+    "author" : 'Akbar Ali',
+    "depends": [
+        'base',
+        'courier',
+    ],
+    "category": "Management",
+    "data": [
+        "data/dashboard_data.xml",
+        "security/dashboard_security.xml",
+        "security/ir.model.access.csv",
+        "views/dashboard_view.xml",
+        "views/res_config_view.xml",
+        "views/assets.xml",
+    ],
+    'installable': True,
+    'licence':'OPL-1',
+}
